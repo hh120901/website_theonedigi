df9defa073dafd296bc360f81240bf4c0ed8c8e9
hh120901 <70877597+hh120901@users.noreply.github.com>
2023-11-20 17:23:53 +0700

    homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,12 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-	<h2 class="text-danger" id="test">HOME PAGE</h2>
-	<script>
-		$(document).ready(function () {
-			$("#test").on('click', function (){
-				console.log('it works');
-			})
-		});
-	</script>
+	<div class="custom-layout">
+		<div class="container position-relative" style="background-image: url({{ asset('assets/images/bgbanner1.png') }}); background-position-y: bottom;">
+			<div style="background-image: url({{ asset('assets/images/bgbanner01.png') }}); background-position-y: inherit; background-repeat: no-repeat; background-position-x: right;">
+				<div class="sub-container">
+					<div class="d-flex flex-wrap">
+						<div class="title w-50">
+							<h2 class="title-banner mb-5">WELCOME <br> TO OUR <br> PLACE!</h2>
+							<p class="fs-5 pe-5 pt-3 fw-semibold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus quo beatae nobis, amet dolore magni dicta quod quia autem voluptatem pariatur.</p>
+						</div>
+						<div class="img-banner w-50 d-flex justify-content-center">
+							<div class="rounded-box d-flex align-items-end justify-content-center overflow-hidden">
+								<img src="{{ asset('assets/images/ava_kevin.png') }}" alt="avt" width="413" height="413">
+							</div>
+						</div>
+					</div>
+					<hr class="my-5">
+					<div class="d-flex">
+						<div class="w-50 pe-5">
+							<p class="fs-5 text-secondary fw-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident consequuntur adipisci reiciendis sed facere necessitatib.</p>
+						</div>
+						<div class="w-50 ps-5">
+							<p class="fs-5 text-secondary fw-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia totam culpa sint minima iste d</p>
+						</div>
+					</div>
+					<div class="d-flex justify-content-center position-relative h-5r">
+						<div class="social-icon d-flex gap-4 align-items-end">
+							<a href="#"><img src="{{ asset('assets/images/FB.svg') }}" alt="FB-icon"></a>
+							<a href="#"><img src="{{ asset('assets/images/linkind.svg') }}" alt="In-icon"></a>
+							<a href="#"><img src="{{ asset('assets/images/Twitter.svg') }}" alt="TW-icon"></a>
+						</div>	
+						<div class="btn-scroll">
+							<button class="btn btn-red-primary position-absolute end-0 fs-4 fw-semibold text-decoration-underline h-100 btn-next-page">
+								SCROLL <img src="{{ asset('assets/images/arrow-right.svg') }}" alt="">
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
--- resources/views/layouts/sections/header.blade.php
+++ resources/views/layouts/sections/header.blade.php
@@ -1,1 +1,22 @@
-<div>HEADER</div>
+<div class="container text-white bg-dark">
+	<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom header-container">
+		<div class="col-md-3 mb-2 mb-md-0">
+		  <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+			<img src="#" alt="Logo Image">
+		  </a>
+		</div>
+	
+		<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+		  <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+		  <li><a href="#" class="nav-link px-2">Features</a></li>
+		  <li><a href="#" class="nav-link px-2">Pricing</a></li>
+		  <li><a href="#" class="nav-link px-2">FAQs</a></li>
+		  <li><a href="#" class="nav-link px-2">About</a></li>
+		</ul>
+	
+		<div class="col-md-3 text-end">
+		  <button type="button" class="btn btn-outline-primary me-2">Login</button>
+		  <button type="button" class="btn btn-primary">Sign-up</button>
+		</div>
+	</header>
+</div>
